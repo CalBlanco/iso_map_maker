0d965490ed23858532c27377eef21597c38c2fba
CalBlanco <72684566+Jimdangle@users.noreply.github.com>
2024-04-17 00:57:22 -0700

    paint tools all seem to be working with walls and objects now

--- core/src/com/isomapmaker/game/controls/commands/LineCommand.java
+++ core/src/com/isomapmaker/game/controls/commands/LineCommand.java
@@ -3,9 +3,10 @@ package com.isomapmaker.game.controls.commands;
 import java.util.Vector;
 
 import com.badlogic.gdx.math.Vector2;
+import com.isomapmaker.game.controls.ModeController;
 import com.isomapmaker.game.controls.PaintTools;
 import com.isomapmaker.game.map.Assets.Asset;
-
+import com.isomapmaker.game.map.Atlas.enums.WallQuadrant;
 import com.isomapmaker.game.map.TileMaps.TileMap;
 
 
@@ -28,10 +29,26 @@ public class LineCommand extends Command{
     private boolean line(){
         Vector<Integer[]> l = PaintTools.line(clickPos, endPos);
         
-
-        for(int i = 0; i<l.size(); i++){
-            map.setFloor(l.get(i)[0], l.get(i)[1],(Asset) floor);
+        switch(ModeController.getInstance().getAssetState()){
+            case Floor:
+                for(int i = 0; i<l.size(); i++){
+                    map.setFloor(l.get(i)[0], l.get(i)[1],floor);
+                }
+            break;
+            case Wall:
+                for(int i = 0; i<l.size(); i++){
+                    map.setWall(l.get(i)[0], l.get(i)[1], WallQuadrant.left, ModeController.getInstance().getWallRegion(WallQuadrant.left));
+                    map.setWall(l.get(i)[0], l.get(i)[1], WallQuadrant.right, ModeController.getInstance().getWallRegion(WallQuadrant.right));
+                    map.setWall(l.get(i)[0], l.get(i)[1], WallQuadrant.top, ModeController.getInstance().getWallRegion(WallQuadrant.top));
+                    map.setWall(l.get(i)[0], l.get(i)[1], WallQuadrant.bottom, ModeController.getInstance().getWallRegion(WallQuadrant.bottom));
+                }
+                break;
+            case Object:
+                for(int i=0; i<l.size(); i++){
+                    map.setObject(l.get(i)[0], l.get(i)[1], floor);
+                }
         }
+        
         return true;
     }
     
